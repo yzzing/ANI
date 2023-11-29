a0569076dd9efb39ade871fcb2ca1f70a8026b63
SeohyunSim <angelsh0805@gachon.ac.kr>
2023-11-30 02:53:48 +0900

    Good_output

--- Item-Based_Collaborative_Filtering.py
+++ Item-Based_Collaborative_Filtering.py
@@ -10,12 +10,14 @@ warnings.filterwarnings('ignore')
 anime = pd.read_csv("anime.csv")
 # print(anime.head())
 
+
+# Preprocessing
 # Select TV, Movie, OVA, ONA
 anime = anime[(anime['type'] == 'TV') | (anime['type'] == 'Movie') | (anime['type'] == 'OVA') | (anime['type'] == 'ONA')]
 
 # Select only famous anime, 75% percentile
-m = anime['members'].quantile(0.75)
-anime = anime[(anime['members'] >= m)]
+f = anime['members'].quantile(0.75)
+anime = anime[(anime['members'] >= f)]
 
 # Read the anime.csv
 rating = pd.read_csv("rating.csv")
@@ -28,23 +30,22 @@ rating.loc[rating.rating == -1, 'rating'] = np.NaN
 
 
 # Index for anime name
-anime_index = pd.Series(anime.index, index=anime.name)
+anime_ind = pd.Series(anime.index, index=anime.name)
 # print(anime_index.head())
 
 
 # Join the data
-joined = anime.merge(rating, how='inner', on='anime_id')
+join = anime.merge(rating, how='inner', on='anime_id')
 # print(joined.head())
 
 
 # Create a pivot table
-joined = joined[['user_id', 'name', 'rating_y']]
-
-pivot = pd.pivot_table(joined, index='name', columns='user_id', values='rating_y')
+join = join[['user_id', 'name', 'rating_y']]
+pivot = pd.pivot_table(join, index='name', columns='user_id', values='rating_y')
 # print(pivot.head())
 
 
-# Drop all users that never rate an anime
+# Drop all users who has no rating
 pivot.dropna(axis=1, how='all', inplace=True)
 # print(pivot.head())
 
@@ -55,44 +56,43 @@ pivot_norm = pivot.apply(lambda x: x - np.nanmean(x), axis=1)
 
 
 # Item Based Collaborative Filtering
-# fill NaN with 0
+# Fill NaN with 0
 pivot_norm.fillna(0, inplace=True)
 # print(pivot_norm.head())
 
 
-# Calculate Similar Items
-# convert into dataframe to make it easier
-item_sim_df = pd.DataFrame(cosine_similarity(pivot_norm, pivot_norm), index=pivot_norm.index, columns=pivot_norm.index)
+# Calculate Similar Animes
+# Convert into dataframe
+item_similar_df = pd.DataFrame(cosine_similarity(pivot_norm, pivot_norm), index=pivot_norm.index, columns=pivot_norm.index)
 # print(item_sim_df.head())
 
 def get_similar_anime(anime_name):
     if anime_name not in pivot_norm.index:
         return None, None
     else:
-        sim_animes = item_sim_df.sort_values(by=anime_name, ascending=False).index[1:]
-        sim_score = item_sim_df.sort_values(by=anime_name, ascending=False).loc[:, anime_name].tolist()[1:]
-        return sim_animes, sim_score
+        similar_animes = item_similar_df.sort_values(by=anime_name, ascending=False).index[1:]
+        similar_score = item_similar_df.sort_values(by=anime_name, ascending=False).loc[:, anime_name].tolist()[1:]
+        return similar_animes, similar_score
 
 
-# Helper Function
-# predict the rating of anime x by user y
+# Predict the rating of anime x by user y
 def predict_rating(user_id, anime_name, max_neighbor=10):
     animes, scores = get_similar_anime(anime_name)
     anime_arr = np.array([x for x in animes])
     sim_arr = np.array([x for x in scores])
     
-    # select only the anime that has already rated by user x
+    # Select the anime that has already rated by user x
     filtering = pivot_norm[user_id].loc[anime_arr] != 0
     
-    # calculate the predicted score
-    s = np.dot(sim_arr[filtering][:max_neighbor], pivot[user_id].loc[anime_arr[filtering][:max_neighbor]]) \
+    # Calculate the predicted score
+    predicted_score = np.dot(sim_arr[filtering][:max_neighbor], pivot[user_id].loc[anime_arr[filtering][:max_neighbor]]) \
             / np.sum(sim_arr[filtering][:max_neighbor])
     
-    return s
+    return predicted_score
 
 
 # Get Recommendation
-# recommend top n_anime for user x based on item collaborative filtering algorithm
+# Recommend top n_anime for user x based on item collaborative filtering algorithm
 def get_recommendation(user_id, n_anime=10):
     print("For user", user_id, ",")
     predicted_rating = np.array([])
@@ -100,12 +100,12 @@ def get_recommendation(user_id, n_anime=10):
     for _anime in pivot_norm.index:
         predicted_rating = np.append(predicted_rating, predict_rating(user_id, _anime))
     
-    # don't recommend something that user has already rated
+    # Exclude the anime that the user has already rated
     temp = pd.DataFrame({'predicted':predicted_rating, 'name':pivot_norm.index})
     filtering = (pivot_norm[user_id] == 0.0)
     temp = temp.loc[filtering.values].sort_values(by='predicted', ascending=False)
 
-    # recommend n_anime anime
-    return anime.loc[anime_index.loc[temp.name[:n_anime]]]
+    # Recommend n_anime anime
+    return anime.loc[anime_ind.loc[temp.name[:n_anime]]]
 
-print(get_recommendation(3))
+print(get_recommendation(7777))
